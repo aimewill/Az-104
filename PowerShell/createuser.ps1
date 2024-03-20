3b2192ef6631121705350f96af07d3e0a7ae74f0
Aimé Souomou <aime.souomou@swisscom.com>
2024-03-20 23:42:28 +0100

    added script

--- PowerShell/createuser.ps1
+++ PowerShell/createuser.ps1
@@ -2,9 +2,5 @@
 # Install-Module AzureAD -Scope CurrentUser -Repository PSGallery -Force
 
 $PasswordProfile = New-Object -TypeName Microsoft.Open.AzureAD.Model.PasswordProfile
-$PasswordProfile.Password = "<Password>"
-New-AzureADUser -DisplayName "New User" -PasswordProfile $C@ssette80* -UserPrincipalName "az104-03a-aaduser1@aimesouomououtlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "Newuser"
-
-ObjectId                             DisplayName UserPrincipalName               UserType
---------                             ----------- -----------------               --------
-5e8b0f4d-2cd4-4e17-9467-b0f6a5c0c4d0 New user    NewUser@contoso.com             Member
+$PasswordProfile.Password = "C@ssette80*"
+New-AzureADUser -DisplayName "az104-01b-aaduser2" -PasswordProfile $PasswordProfile -UserPrincipalName "az104-01b-aaduser2@aimesouomououtlook.onmicrosoft.com" -AccountEnabled $true -MailNickName "Newuserb"
